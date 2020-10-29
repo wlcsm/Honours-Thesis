80d0e20084b7225192a30db33cfb1d7f08169a3e
willcsm <wcashman1351@gmail.com>
2020-10-29 15:48:06 +1100

    Update

--- plot.py
+++ plot.py
@@ -7,40 +7,36 @@ kronecker = pd.read_csv("kronecker.txt", delimiter=' ')
 fft = pd.read_csv("fft.txt", delimiter=' ')
 schoolbook = pd.read_csv("schoolbook.txt", delimiter=' ')
 
-# The par_iter vs iter
 karatsuba.plot(x      = "degree", 
           y      = ["time"],
           kind   = "line", 
           legend = False,
           xlabel = "Degree",  
-          ylabel = "Time (μs)", 
-          title  = "Karatsuba's Multiplication")
+          ylabel = "Time (s)", 
+          title  = "Karatsuba Multiplication")
 
-# The par_iter vs iter
 schoolbook.plot(x      = "degree", 
           y      = ["1-var", "2-var", "5-var",  "10-var"],
           kind   = "line", 
           xlabel = "Number of non-zero terms",  
-          ylabel = "Time (μs)", 
+          ylabel = "Time (s)", 
           title  = "Schoolbook Multiplication")
 
-# The benchmarks
 kronecker.plot(x      = "degree", 
           y      = ["2-var", "5-var",  "10-var"],
           kind   = "line", 
           xlabel = "Number of non-zero terms",  
-          ylabel = "Time (μs)", 
-          # rot    = 20,
+          ylabel = "Time (s)", 
           title  = "Kronecker Substitution")
 
 fft.plot(x      = "degree", 
           y      = ["time"],
           kind   = "line", 
           legend = False,
+          # logx = True,
           xlabel = "Degree",  
-          ylabel = "Time (μs)", 
-          # rot    = 20,
-          title  = "FFT-based Polynomial Multiplication")
+          ylabel = "Time (s)", 
+          title  = "FFT-based Multiplication")
 
 plt.show()
 
